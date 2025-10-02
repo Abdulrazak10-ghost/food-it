9e1ce85f1172819c277efb070fb7a56691136a98
Abdulrazak10-ghost <Itzrixn@gmail.com>
2025-10-02 14:57:43 +0100

    Initial commit - food delivery project

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite Dish here.</h2>
+            <p>Choose from a variety of local and inter-continetal dishes to bring your tste buds to life and fulfil your cravings.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
